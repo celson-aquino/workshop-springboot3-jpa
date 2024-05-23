7d74ea53649b26fde9cf27dd148f00729c524d65
Celson Nonato Cruz de Aquino <celson.aquino@gmail.com>
2024-05-23 12:28:04 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -40,7 +40,6 @@ public class OrderItemPK implements Serializable {
 	public int hashCode() {
 		return Objects.hash(order, product);
 	}
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -52,5 +51,5 @@ public class OrderItemPK implements Serializable {
 		OrderItemPK other = (OrderItemPK) obj;
 		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
 	}
-
+	
 }
